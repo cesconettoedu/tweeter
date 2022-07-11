c02658bb3e8cf919708da5d0e38f7e65209bf99b
duducesconetto <cesconettoedu@gmail.com>
2022-07-11 15:23:23 +0000

    implement the character counter

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,13 +1,22 @@
 $(document).ready(function() {
   
-  const newT = document.getElementsByClassName('new-tweet');
+  $("#tweet-text").on("input", function(){
 
+    const maxChars = 140;
 
+    
+    const inputLength = $(this).val().length;
+    const restNum = maxChars - inputLength
+    const $quantityNum = $('.counter');
+   
+    $quantityNum.text(restNum);
+
+    if (restNum < 0) {
+      $quantityNum.css('color', 'red');
+    } else {
+      $quantityNum.css('color', '#545149');
+    }
+
+  })
 
 });
-
-
-
-
-
-
